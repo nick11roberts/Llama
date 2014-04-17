c80339fdd7a526a1bd33455f3c61433216f135d8
nick11robers <nick.roberts.127.0.0.1@gmail.com>
2014-04-16 19:24:01 -0700

    Started the project thing. It's time to start Llama-ing.

---
+++ LlamaSpawningButtonThing/gen/io/github/nick11roberts/llamaspawningbuttonthing/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package io.github.nick11roberts.llamaspawningbuttonthing;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
