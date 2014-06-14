1680902511d83b170ecb28dba2954b9f245799cc
Nicholas Roberts <nick.roberts.127.0.0.1@gmail.com>
2014-06-14 02:08:57 -0700

    Some llamas go off the window. Other than that, done.

--- LlamaSpawningButtonThing/gen/io/github/nick11roberts/llamaspawningbuttonthing/R.java
+++ LlamaSpawningButtonThing/gen/io/github/nick11roberts/llamaspawningbuttonthing/R.java
@@ -1078,8 +1078,9 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int button=0x7f020057;
-        public static final int ic_launcher=0x7f020058;
+        public static final int actual_llama=0x7f020057;
+        public static final int button=0x7f020058;
+        public static final int ic_launcher=0x7f020059;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1093,7 +1094,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003e;
+        public static final int action_settings=0x7f05003f;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1121,6 +1122,7 @@ containing a value of this type.
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int relLayoutMain=0x7f05003c;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1138,8 +1140,8 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
-        public static final int theButtonThing=0x7f05003c;
-        public static final int theButtonThingFragment=0x7f05003d;
+        public static final int theButtonThing=0x7f05003d;
+        public static final int theButtonThingFragment=0x7f05003e;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
--- LlamaSpawningButtonThing/src/io/github/nick11roberts/llamaspawningbuttonthing/MainActivity.java
+++ LlamaSpawningButtonThing/src/io/github/nick11roberts/llamaspawningbuttonthing/MainActivity.java
@@ -1,91 +1,139 @@
 package io.github.nick11roberts.llamaspawningbuttonthing;
 
+import java.util.ArrayList;
+import java.util.List;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Point;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.Display;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
-import android.os.Build;
 
 public class MainActivity extends ActionBarActivity {
-
-	ImageButton myNiceButton;
+	
+	private static boolean DEBUG = true;
+	private static int IMAGE_HEIGHT = 120;
+	private static int MIN_ROTATION = -60;
+	private static int MAX_ROTATION = 60;
+	private int min;
+	private int maxWidth; 
+	private int maxHeight; 
+	private double maxWidthDisplacement;
+	private double maxHeightDisplacement;
+	private RelativeLayout mainLayout;
+	private ImageButton myNiceButton;
+	private List<ImageView> llamaProscriptionList = new ArrayList<ImageView>();
+	private int indexOfLlamaProscriptionList = 0;
+	private Context c = this;
+	private RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(IMAGE_HEIGHT, IMAGE_HEIGHT);
+	//private Display display = getWindowManager().getDefaultDisplay();
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        maxWidthDisplacement = findScreenWidth() * .1;
+        maxHeightDisplacement = findScreenHeight() * .1;
+        
+        min = /*(int) maxWidthDisplacement*/240;
+        maxWidth = (int) (findScreenWidth()-maxWidthDisplacement);
+        maxHeight = /*(int) (findScreenHeight()-maxHeightDisplacement)*/1200;
+        
+        
+        mainLayout = (RelativeLayout)findViewById(R.id.relLayoutMain);
+        
         myNiceButton = (ImageButton)findViewById(R.id.theButtonThing); 
-
-        /*if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }*/
+        
         
         addListenerOnButton();
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    
-    /*public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }*/
     
     public void addListenerOnButton(){
-    	
-        
+    	        
     	myNiceButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View myNiceButton) {
- 
-			   Toast.makeText(MainActivity.this,
-				"ImageButton is clicked!", Toast.LENGTH_SHORT).show();
+				
+				
+				ImageView newestLlama = new ImageView(c);
+				newestLlama.setImageResource(R.drawable.actual_llama);
+				newestLlama.setLayoutParams(layoutParams);
+				newestLlama.setX(min 
+						+ (int)(Math.random() * ((maxWidth - min) + 1)));
+				newestLlama.setY(min 
+						+ (int)(Math.random() * ((maxHeight - min) + 1)));
+				newestLlama.setPivotX(newestLlama.getX());
+				newestLlama.setPivotY(newestLlama.getY());
+				newestLlama.setRotation(MIN_ROTATION 
+						+ (int)(Math.random() * ((MAX_ROTATION - MIN_ROTATION) + 1)));
+				
+				if(DEBUG){
+					System.out.println(" -- ");
+					System.out.println("Screen dimensions: " + findScreenWidth() 
+							+ " X " + findScreenHeight());
+					System.out.println("maxWidth = " + maxWidth);
+					System.out.println("maxHeight = " + maxHeight);
+					System.out.println("getX = " + newestLlama.getX());
+					System.out.println("getY = " + newestLlama.getY());
+					System.out.println(" -- ");
+				}
+				
+				llamaProscriptionList.add(newestLlama);
+				mainLayout.addView(llamaProscriptionList.get(indexOfLlamaProscriptionList));
+				indexOfLlamaProscriptionList++;
+				
  
 			}
  
 		});
+    	
+    	
         	
     }
+    
+    @SuppressLint("NewApi")
+	@SuppressWarnings("deprecation")
+	int findScreenWidth(){
+    	int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+    	Display display = getWindowManager().getDefaultDisplay();
+    	Point size = new Point();
+    	int width;
+    	
+    	if (currentapiVersion >= 13){
+	    	display.getSize(size);
+	    	width = size.x;
+    	}
+    	else{
+    		width = display.getWidth();
+    	}
+    	return width;
+    }
+    
+    @SuppressLint("NewApi")
+	@SuppressWarnings("deprecation")
+	int findScreenHeight(){
+    	int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+    	Display display = getWindowManager().getDefaultDisplay();
+    	Point size = new Point();
+    	int height;
+    	
+    	if (currentapiVersion >= 13){
+	    	display.getSize(size);
+	    	height = size.y;
+    	}
+    	else{
+    		height = display.getHeight();
+    	}
+    	return height;
+    }
+    
     
 
 }
