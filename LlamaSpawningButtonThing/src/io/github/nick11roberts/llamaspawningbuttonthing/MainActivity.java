a7bef9615316477d80c587a97dbbfbeab42a57d2
nick11robers <nick.roberts.127.0.0.1@gmail.com>
2014-06-14 17:41:32 -0700

    Done, version 1.0 has been released.

--- LlamaSpawningButtonThing/src/io/github/nick11roberts/llamaspawningbuttonthing/MainActivity.java
+++ LlamaSpawningButtonThing/src/io/github/nick11roberts/llamaspawningbuttonthing/MainActivity.java
@@ -2,50 +2,35 @@ package io.github.nick11roberts.llamaspawningbuttonthing;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
 import android.support.v7.app.ActionBarActivity;
-import android.annotation.SuppressLint;
 import android.content.Context;
-import android.graphics.Point;
 import android.os.Bundle;
-import android.view.Display;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 	
-	private static boolean DEBUG = true;
 	private static int IMAGE_HEIGHT = 120;
 	private static int MIN_ROTATION = -60;
 	private static int MAX_ROTATION = 60;
-	private int min;
-	private int maxWidth; 
-	private int maxHeight; 
-	private double maxWidthDisplacement;
-	private double maxHeightDisplacement;
+	
 	private RelativeLayout mainLayout;
 	private ImageButton myNiceButton;
 	private List<ImageView> llamaProscriptionList = new ArrayList<ImageView>();
 	private int indexOfLlamaProscriptionList = 0;
 	private Context c = this;
-	private RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(IMAGE_HEIGHT, IMAGE_HEIGHT);
-	//private Display display = getWindowManager().getDefaultDisplay();
+	private RelativeLayout.LayoutParams layoutParams;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        maxWidthDisplacement = findScreenWidth() * .1;
-        maxHeightDisplacement = findScreenHeight() * .1;
-        
-        min = /*(int) maxWidthDisplacement*/240;
-        maxWidth = (int) (findScreenWidth()-maxWidthDisplacement);
-        maxHeight = /*(int) (findScreenHeight()-maxHeightDisplacement)*/1200;
-        
-        
+
         mainLayout = (RelativeLayout)findViewById(R.id.relLayoutMain);
         
         myNiceButton = (ImageButton)findViewById(R.id.theButtonThing); 
@@ -61,33 +46,47 @@ public class MainActivity extends ActionBarActivity {
 			@Override
 			public void onClick(View myNiceButton) {
 				
+				DisplayMetrics metrics = new DisplayMetrics();
+				getWindowManager().getDefaultDisplay().getMetrics(metrics);
 				
+				
+				///// SET HEIGHT LAYOUT PARAMETER
+				layoutParams = new RelativeLayout.LayoutParams(IMAGE_HEIGHT, IMAGE_HEIGHT);
+				
+				///// INSTANTIATE LLAMA
 				ImageView newestLlama = new ImageView(c);
-				newestLlama.setImageResource(R.drawable.actual_llama);
-				newestLlama.setLayoutParams(layoutParams);
-				newestLlama.setX(min 
-						+ (int)(Math.random() * ((maxWidth - min) + 1)));
-				newestLlama.setY(min 
-						+ (int)(Math.random() * ((maxHeight - min) + 1)));
-				newestLlama.setPivotX(newestLlama.getX());
-				newestLlama.setPivotY(newestLlama.getY());
-				newestLlama.setRotation(MIN_ROTATION 
-						+ (int)(Math.random() * ((MAX_ROTATION - MIN_ROTATION) + 1)));
 				
-				if(DEBUG){
-					System.out.println(" -- ");
-					System.out.println("Screen dimensions: " + findScreenWidth() 
-							+ " X " + findScreenHeight());
-					System.out.println("maxWidth = " + maxWidth);
-					System.out.println("maxHeight = " + maxHeight);
-					System.out.println("getX = " + newestLlama.getX());
-					System.out.println("getY = " + newestLlama.getY());
-					System.out.println(" -- ");
-				}
 				
+				//// SET SPECIFIC LLAMA IMAGE
+				(newestLlama).setImageResource(R.drawable.actual_llama);
+				
+				
+				///// ROTATION FOR SPECIFIC LLAMA ---- SEEMS INDEPENDANT
+				(newestLlama)
+					.setPivotX((newestLlama).getX());
+				(newestLlama)
+					.setPivotY((newestLlama).getY());
+				(newestLlama)
+					.setRotation(MIN_ROTATION + (int)(Math.random() * ((MAX_ROTATION - MIN_ROTATION) + 1)));
+				
+				///// POSITION SPECIFIC LLAMA ---- SEEMS DEPENDANT 
+				layoutParams.leftMargin = new Random().nextInt(metrics.widthPixels 
+					- (newestLlama).getWidth());
+				layoutParams.topMargin = new Random().nextInt(metrics.heightPixels 
+					- 2*(newestLlama).getHeight());
+				
+				
+				//// ADD TO LLAMA LIST
 				llamaProscriptionList.add(newestLlama);
+				
+				//// SET PARAMETERS
+				(llamaProscriptionList.get(indexOfLlamaProscriptionList)).setLayoutParams(layoutParams);
+				
+				///// ADD VIEW
 				mainLayout.addView(llamaProscriptionList.get(indexOfLlamaProscriptionList));
-				indexOfLlamaProscriptionList++;
+				
+				// INCREMENT COUNTER
+				indexOfLlamaProscriptionList+=1;
 				
  
 			}
@@ -98,41 +97,6 @@ public class MainActivity extends ActionBarActivity {
         	
     }
     
-    @SuppressLint("NewApi")
-	@SuppressWarnings("deprecation")
-	int findScreenWidth(){
-    	int currentapiVersion = android.os.Build.VERSION.SDK_INT;
-    	Display display = getWindowManager().getDefaultDisplay();
-    	Point size = new Point();
-    	int width;
-    	
-    	if (currentapiVersion >= 13){
-	    	display.getSize(size);
-	    	width = size.x;
-    	}
-    	else{
-    		width = display.getWidth();
-    	}
-    	return width;
-    }
-    
-    @SuppressLint("NewApi")
-	@SuppressWarnings("deprecation")
-	int findScreenHeight(){
-    	int currentapiVersion = android.os.Build.VERSION.SDK_INT;
-    	Display display = getWindowManager().getDefaultDisplay();
-    	Point size = new Point();
-    	int height;
-    	
-    	if (currentapiVersion >= 13){
-	    	display.getSize(size);
-	    	height = size.y;
-    	}
-    	else{
-    		height = display.getHeight();
-    	}
-    	return height;
-    }
     
     
 
