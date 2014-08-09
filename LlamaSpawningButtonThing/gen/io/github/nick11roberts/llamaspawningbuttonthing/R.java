e86333076a03dc148f37ba4c22e676f31f313291
nick11robers <nick.roberts.127.0.0.1@gmail.com>
2014-08-09 16:56:21 -0700

    Llama transparent webview is active. Problems with Llama Web scaling, however...

--- LlamaSpawningButtonThing/gen/io/github/nick11roberts/llamaspawningbuttonthing/R.java
+++ LlamaSpawningButtonThing/gen/io/github/nick11roberts/llamaspawningbuttonthing/R.java
@@ -1094,7 +1094,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003e;
+        public static final int action_settings=0x7f05003f;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1115,6 +1115,7 @@ containing a value of this type.
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
+        public static final int llamaBrowser=0x7f05003e;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
@@ -1198,7 +1199,8 @@ containing a value of this type.
         public static final int abc_search_view=0x7f030016;
         public static final int abc_simple_decor=0x7f030017;
         public static final int activity_llama=0x7f030018;
-        public static final int support_simple_spinner_dropdown_item=0x7f030019;
+        public static final int activity_llama_web=0x7f030019;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001a;
     }
     public static final class menu {
         public static final int main=0x7f0c0000;
--- LlamaSpawningButtonThing/src/io/github/nick11roberts/llamaspawningbuttonthing/LlamaActivity.java
+++ LlamaSpawningButtonThing/src/io/github/nick11roberts/llamaspawningbuttonthing/LlamaActivity.java
@@ -1,101 +1,48 @@
 package io.github.nick11roberts.llamaspawningbuttonthing;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 import android.support.v7.app.ActionBarActivity;
-import android.content.Context;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.view.View;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
 public class LlamaActivity extends ActionBarActivity {
 	
-	private static int IMAGE_HEIGHT = 120;
-	private static int MIN_ROTATION = -60;
-	private static int MAX_ROTATION = 60;
 	
-	private RelativeLayout mainLayout;
-	private ImageButton myNiceButton;
-	private List<ImageView> llamaProscriptionList = new ArrayList<ImageView>();
-	private int indexOfLlamaProscriptionList = 0;
-	private Context c = this;
-	private RelativeLayout.LayoutParams layoutParams;
 	
-    @Override
+    @SuppressLint("SetJavaScriptEnabled")
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_llama);
-
-        mainLayout = (RelativeLayout)findViewById(R.id.relLayoutMain);
-        
-        myNiceButton = (ImageButton)findViewById(R.id.theButtonThing); 
+        setContentView(R.layout.activity_llama_web);
         
         
-        addListenerOnButton();
+        final WebView localLlamaEmbed = (WebView)findViewById(R.id.llamaBrowser);   
+        localLlamaEmbed.getSettings().setJavaScriptEnabled(true);  
+          
+//        /* WebViewClient must be set BEFORE calling loadUrl! */  
+//        webview.setWebViewClient(new WebViewClient() {  
+//            @Override  
+//            public void onPageFinished(WebView view, String url)  
+//            {  
+//                webview.loadUrl("javascript:(function() { " +  
+//                        "document.getElementsByTagName('body')[0].style.color = 'red'; " +  
+//                        "})()");  
+//            }  
+//        });  
+          
+        
+        localLlamaEmbed.setBackgroundColor(0x00000000);
+        localLlamaEmbed.setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
+        
+        localLlamaEmbed.loadUrl("file:///android_asset/web/Llama-Web/index.html");  
+        
+        
+        
     }
 
     
-    public void addListenerOnButton(){
-    	        
-    	myNiceButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View myNiceButton) {
-				
-				DisplayMetrics metrics = new DisplayMetrics();
-				getWindowManager().getDefaultDisplay().getMetrics(metrics);
-				
-				
-				///// SET HEIGHT LAYOUT PARAMETER
-				layoutParams = new RelativeLayout.LayoutParams(IMAGE_HEIGHT, IMAGE_HEIGHT);
-				
-				///// INSTANTIATE LLAMA
-				ImageView newestLlama = new ImageView(c);
-				
-				
-				//// SET SPECIFIC LLAMA IMAGE
-				(newestLlama).setImageResource(R.drawable.actual_llama);
-				
-				
-				///// ROTATION FOR SPECIFIC LLAMA ---- SEEMS INDEPENDANT
-				(newestLlama)
-					.setPivotX((newestLlama).getX());
-				(newestLlama)
-					.setPivotY((newestLlama).getY());
-				(newestLlama)
-					.setRotation(MIN_ROTATION + (int)(Math.random() * ((MAX_ROTATION - MIN_ROTATION) + 1)));
-				
-				///// POSITION SPECIFIC LLAMA ---- SEEMS DEPENDANT 
-				layoutParams.leftMargin = new Random().nextInt(metrics.widthPixels 
-					- (newestLlama).getWidth());
-				layoutParams.topMargin = new Random().nextInt(metrics.heightPixels 
-					- 2*(newestLlama).getHeight());
-				
-				
-				//// ADD TO LLAMA LIST
-				llamaProscriptionList.add(newestLlama);
-				
-				//// SET PARAMETERS
-				(llamaProscriptionList.get(indexOfLlamaProscriptionList)).setLayoutParams(layoutParams);
-				
-				///// ADD VIEW
-				mainLayout.addView(llamaProscriptionList.get(indexOfLlamaProscriptionList));
-				
-				// INCREMENT COUNTER
-				indexOfLlamaProscriptionList+=1;
-				
- 
-			}
- 
-		});
-    	
-    	
-        	
-    }
+    
     
     
     
